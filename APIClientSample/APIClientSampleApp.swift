80d332fff6d9c56431ad13525308e940a99dea05
Daiki Fujimori <G016C1102@outlook.jp>
2025-08-23 22:17:40 +0900

    マルチモジュールの雛形作成

--- APIClientSample/APIClientSampleApp.swift
+++ APIClientSample/APIClientSampleApp.swift
@@ -5,7 +5,6 @@
 //  Created by Daiki Fujimori on 2025/08/23
 //  
 
-
 import SwiftUI
 
 @main
---
+++ APIModule/Sources/Domain/APIRequesting.swift
@@ -0,0 +1,10 @@
+//
+//  APIRequesting.swift
+//  APIClientSample
+//  
+//  Created by Daiki Fujimori on 2025/08/23
+//  
+
+public protocol APIRequesting: Sendable {}
+
+public protocol Endpoint {}
