2ef0daad36fb713cc49b4ac854e3ff87e689f1c7
Daiki Fujimori <G016C1102@outlook.jp>
2025-08-24 18:56:21 +0900

    DependencyValues追加

---
+++ APIClientSample/Core/Dependency/DependencyValues.swift
@@ -0,0 +1,75 @@
+//
+//  DependencyValues.swift
+//  APIClientSample
+//  
+//  Created by Daiki Fujimori on 2025/08/24
+//
+
+protocol DependencyKey {
+    
+    associatedtype Value: Sendable
+    static var liveValue: Value { get }
+}
+
+struct DependencyValues: Sendable {
+    
+    @TaskLocal fileprivate static var current = Self()
+    private var storage: [ObjectIdentifier: any Sendable] = [:]
+    
+    private init() {}
+    
+    subscript<K>(key: K.Type) -> K.Value where K: DependencyKey {
+        
+        get {
+            
+            guard let base = storage[ObjectIdentifier(key)],
+                  let dependency = base as? K.Value else {
+                
+                return key.liveValue
+            }
+            return dependency
+        }
+        
+        set {
+            
+            storage[ObjectIdentifier(key)] = newValue
+        }
+    }
+}
+
+extension DependencyValues {
+    
+    static func withDependency<R>(
+        _ setDependency: (inout DependencyValues) -> Void,
+        operation: () -> R) -> R {
+        
+        var currentDependencyValue = DependencyValues.current
+        setDependency(&currentDependencyValue)
+        
+        return DependencyValues.$current.withValue(currentDependencyValue) { operation() }
+    }
+    
+    static func withDependency<R>(
+        _ setDependency: (inout DependencyValues) -> Void,
+        operation: () async -> R
+    ) async -> R {
+        
+        var currentDependencyValue = DependencyValues.current
+        setDependency(&currentDependencyValue)
+        
+        return await DependencyValues.$current.withValue(currentDependencyValue) { await operation() }
+    }
+}
+
+@propertyWrapper
+struct Dependency<Value> {
+    
+    private let keyPath: KeyPath<DependencyValues, Value> & Sendable
+
+    init(_ keyPath: KeyPath<DependencyValues, Value> & Sendable) {
+        
+        self.keyPath = keyPath
+    }
+
+    var wrappedValue: Value { DependencyValues.current[keyPath: keyPath] }
+}
